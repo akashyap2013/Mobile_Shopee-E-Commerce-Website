e86de4c520e5fba03b477caa8f9ec61110064dea
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2020-04-15 20:22:54 +0530

    Special Price Added

--- index.js
+++ index.js
@@ -24,4 +24,17 @@ $(document).ready(function(){
         }
     });
 
+    // isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector : '.grid-item',
+        layoutMode : 'fitRows'
+    });
+
+    // filter items on button click
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue});
+    })
+
+
 });
