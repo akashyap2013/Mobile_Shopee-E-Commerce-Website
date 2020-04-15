0471018a89fbd14973125f31719555723b832b6f
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2020-04-15 19:38:21 +0530

    banner area added

---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    // banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1
+    });
+
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
+});
