61a5540c57a47464304816a8cfa600683845cb57
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2020-04-18 21:00:48 +0530

    product.html Added

--- index.js
+++ index.js
@@ -71,4 +71,28 @@ $(document).ready(function(){
     })
 
 
+    // product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    let $input = $(".qty .qty_input");
+
+    // click on qty up button
+    $qty_up.click(function(e){
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            });
+        }
+    });
+
+       // click on qty down button
+       $qty_down.click(function(e){
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            });
+        }
+    });
+
+
 });
