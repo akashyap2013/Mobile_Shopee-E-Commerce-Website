7120c164aa4fd51b22bd6658c2394ed222217e7f
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2020-04-19 20:12:30 +0530

    Cart Added

--- index.js
+++ index.js
@@ -74,10 +74,11 @@ $(document).ready(function(){
     // product qty section
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
-    let $input = $(".qty .qty_input");
+    // let $input = $(".qty .qty_input");
 
     // click on qty up button
     $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() >= 1 && $input.val() <= 9){
             $input.val(function(i, oldval){
                 return ++oldval;
@@ -87,6 +88,7 @@ $(document).ready(function(){
 
        // click on qty down button
        $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() > 1 && $input.val() <= 10){
             $input.val(function(i, oldval){
                 return --oldval;
